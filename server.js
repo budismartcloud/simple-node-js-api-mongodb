726a3cea2ca228d440f49dce0190606c6703b14a
Budi Santoso <budisantoso@smartcloud.id>
2018-06-25 16:30:51 +0700

    First init

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+var app = express(); 
+app.use(bodyParser.urlencoded({ extended: true})); 
+app.use(bodyParser.json()); 
+var dbConfig = require('./config/database.config.js'); 
+var mongoose = require('mongoose');  
+mongoose.connect(dbConfig.url, { useMongoClient: true }); 
+mongoose.connection.on('error', function(){     
+	console.log('Could not connect to the database. Exiting now...');     
+	process.exit(); 
+}); 
+ 
+mongoose.connection.once('open', function(){     
+	console.log('Successfully connected to the database.'); 
+}); 
+ 
+
+app.get('/', function(req, res){     
+	res.json({"message": "Welcome to My Online Furniture Store."}); 
+}); 
+ 
+require('./app/routes/product.routes.js')(app); 
+  
+app.listen(3000, function(){     
+	console.log("Server is listening on port 3000"); 
+});
+
